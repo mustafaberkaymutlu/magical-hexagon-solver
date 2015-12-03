525fd1e7e7a68c8fdff6a4e8d36ebdff72408306
Mustafa Berkay Mutlu <mustafaberkaymutlu@gmail.com>
2015-12-03 12:16:51 +0200

    Test mode dan cikartildi ve son hali verildi.
    Former-commit-id: 34d9631ba9843e7392d27b7a244cef03d5dcf117

--- YapayZekaOdevi2/Config.cs
+++ YapayZekaOdevi2/Config.cs
@@ -4,14 +4,14 @@ namespace YapayZekaOdevi2
     public static class Config
     {
         // Points if the testing mode is on or off.
-        public static readonly bool TEST_MODE = true;
+        public static readonly bool TEST_MODE = false;
 
         // The count of iteration when test mode is on.
         public static readonly int TEST_MODE_ITERATION_COUNT = 100;
 
         // If you chose Config.PROCESSED_BOARDS_UPPER_LIMIT higher than the count of 
         // all neighbors a board can have, than GetHighestNeighbor(..) may fail.
-        public static readonly ushort PROCESSED_BOARDS_UPPER_LIMIT = ushort.MaxValue;
+        public static readonly ushort PROCESSED_BOARDS_UPPER_LIMIT = 10;
 
         // If MAXIMUM_ITERATION_COUNT is reached and still couldn't found any solution,
         // than operation stops with result of solutionIsFound=false;
